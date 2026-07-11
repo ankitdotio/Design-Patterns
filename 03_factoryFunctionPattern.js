b0e47d6a1d141fa460b63c7b4eaa830128dff668
Ankit3k2 <141257816+Ankit3k2@users.noreply.github.com>
2026-07-11 20:40:48 +0530

    updated

--- 03_factoryFunctionPattern.js
+++ 03_factoryFunctionPattern.js
@@ -1,12 +1,20 @@
-function createUser(name, age) {
+function createUser(name, id, course) {
+  let verified = false;
   return {
     greet() {
       console.log(`hello this is ${name}`);
     },
-    getAge() {
-      console.log(`Age is ${age}`);
+    getId() {
+      console.log(`Id is ${id}`);
+    },
+    verifyUser() {
+      verified = true;
+      console.log("USER IS NOW VERIFIED");
+    },
+    isVerified() {
+      console.log(`IS USER VERIFIED : ${verified}`);
     },
   };
 }
 
-user1 = createUser("Ankit", 22);
+user1 = createUser("Ankit", 22, "cse");
