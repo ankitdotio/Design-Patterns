e1865a057c1baf8d8ed1a4f938892d332e8dc9a7
Ankit3k2 <141257816+Ankit3k2@users.noreply.github.com>
2026-07-12 12:34:46 +0530

    lazyloading

---
+++ lazyLoading/wtihIntersectionObserver.js
@@ -0,0 +1,18 @@
+const img = document.querySelectorAll("img");
+
+const observer = new IntersectionObserver(
+  function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        img.src = entry.dataset.src;
+        img.classList.add("loaded");
+        observer.unobserve(entry);
+      }
+    });
+  },
+  {
+    root: null,
+    threshold: 0.1,
+  },
+);
